0a18aa2bbef188bba34467e5f1db8114d17ecad3
kos4597 <kos4597@naver.com>
2025-03-17 19:35:52 +0900

    리소스 매니저 추가

---
+++ Assets/Scripts/Util/ResourceManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public static class ResourceManager
+{
+    public static GameObject CreateGameObject(string path, Transform parentTr = null)
+    {
+        GameObject go = Resources.Load<GameObject>(path);
+        if(go == null)
+        {
+            Debug.LogError($"{path} 프리팹을 로드할 수 없습니다.");
+            return null;
+        }
+
+        return GameObject.Instantiate(go, parentTr);
+    }
+
+    public static GameObject CreateGameObject(string path, Vector3 position, Quaternion rotation)
+    {
+        GameObject go = Resources.Load<GameObject>(path);
+        if (go == null)
+        {
+            Debug.LogError($"{path} 프리팹을 로드할 수 없습니다.");
+            return null;
+        }
+
+        return GameObject.Instantiate(go, position, rotation);
+    }
+}
